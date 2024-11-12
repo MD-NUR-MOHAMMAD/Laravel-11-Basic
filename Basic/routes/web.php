31a6ffa1c8af0ccf341d3864c4ea3d97a6fd9b3f
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-11-12 13:28:13 +0600

    Show user data properly

--- Basic/routes/web.php
+++ Basic/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CrudController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,6 +14,8 @@ Route::middleware('admin')->group(function () {
     Route::post('/crud/add', [CrudController::class, 'store'])->name('crud.store');
     Route::get('/crud/add', [CrudController::class, 'create'])->name('crud.create');
     Route::get('/crud/edit', [CrudController::class, 'edit'])->name('crud.edit');
+    Route::get('/users', [UserController::class, 'index'])->name('users');
+
 });
 
 
