e0cb233a5653002fa0a4607893cf9c54dfc88bc4
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-11-18 16:54:11 +0600

    category create

--- Basic/app/Http/Controllers/CategoryController.php
+++ Basic/app/Http/Controllers/CategoryController.php
@@ -24,7 +24,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        echo "Categories Create";
+       return view('categories.create');
     }
 
     /**
