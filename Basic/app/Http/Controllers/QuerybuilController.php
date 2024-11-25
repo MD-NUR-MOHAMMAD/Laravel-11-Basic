6f07db62e896d881ccec48cc98c7b23ef59656bc
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-11-25 15:03:11 +0600

    Raw query added

--- Basic/app/Http/Controllers/QuerybuilController.php
+++ Basic/app/Http/Controllers/QuerybuilController.php
@@ -7,8 +7,21 @@ use Illuminate\Support\Facades\DB;
 
 class QuerybuilController extends Controller
 {
-    public function dbone(){
-        $users = DB::table('users')->get();
-        dd($users);
+    public function dbone()
+    {
+        // $users = DB::table('users')->get();
+        $users = DB::select('select * from users');
+
+        $categories = DB::table('categories')
+            ->select('id', 'name')
+            ->where('status', 0)
+            ->where('id', '>', 4)
+            ->orderBy('id', 'desc')
+            ->get();
+        $subcat = DB::table('sub_categories')
+            ->select('category_id')
+            ->distinct()
+            ->get();
+        dd($categories);
     }
 }
