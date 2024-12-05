eccfe7ad9de82bdd1002b5e31e39bcb11d447b17
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-12-05 12:22:25 +0600

    scope for query record]

--- Basic/app/Models/Crud.php
+++ Basic/app/Models/Crud.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Crud extends Model
 {
     use HasFactory;
+
+    // Scopes code is mine is not good but it the way to do it
+    public function scopeComplete($query)
+    {
+        return $query->where('name', '<', 10);
+    }
+    public function scopeInComplete($query)
+    {
+        return $query->where('name', '>', 0);
+    }
 }
