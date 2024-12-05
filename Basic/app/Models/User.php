072c070a3038e986da974fbfcefb224140329121
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-12-05 12:34:47 +0600

    Acceror mutator and field name modify

--- Basic/app/Models/User.php
+++ Basic/app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -12,6 +14,26 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * Interact with the user's  name.
+     */
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => strtoupper($value),
+            // set: fn (string $value) => password_hash($value, PASSWORD_DEFAULT),
+            set: fn (string $value) => strtolower($value),
+        );
+    }
+    /**
+     * Interact with the user's  name and email.
+     */
+    protected function getnameemailAttribute()
+    {
+        return $this->name . " ( " . $this->email. " ) ";
+
+    }
 
     /**
      * The attributes that are mass assignable.
